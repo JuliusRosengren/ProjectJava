9779f51a464bcae9059eafb718f20b563528c197
Julius <djulius.rosengren@gmail.com>
2022-02-19 17:28:27 +0100

    l

---
+++ Blackjack.js
@@ -0,0 +1,19 @@
+let images = ["Extra/k2.webp",
+"Extra/k3.webp",
+"Extra/k4.webp",
+"Extra/k5.webp",
+"Extra/k6.webp",
+"Extra/k7.webp",
+"Extra/k8.webp",
+"Extra/k9.webp",
+"Extra/k10.webp",
+"Extra/kj.webp",
+"Extra/kq.webp",
+"Extra/kk.webp",
+"Extra/ka.webp"];
+
+let card = document.querySelectorAll("img");
+
+function deal(){
+    
+}
