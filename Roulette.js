a68c42e53b313105500440d308755894a4b36353
Julius <djulius.rosengren@gmail.com>
2022-02-26 21:47:57 +0100

    yes

--- Roulette.js
+++ Roulette.js
@@ -1,11 +1,19 @@
 let Money = 100;
 let TimesWon = 0;
 let TimesLost = 0; 
+let Amount = 0;
+let Odd = 0;
+let Even = 0; 
+let Zero = 0;
+
 
 let MoneyText = document.querySelector(".Money");
-let TimesWonText = document.querySelector(".TimesWon")
-let TimesLostText = document.querySelector(".TimesLost")
+let TimesWonText = document.querySelector(".TimesWon");
+let TimesLostText = document.querySelector(".TimesLost");
 
+let OddText = document.querySelector("#Odd");
+let EvenText = document.querySelector("#Even");
+let ZeroText = document.querySelector("#Zero");
 
 let images = ["Extra/wheel.png"];
 let wheel = document.querySelectorAll("img");
@@ -20,6 +28,8 @@ function spin(){
         });
         let spinOneValue = Math.floor(Math.random()*37);
         document.querySelector("#total").innerHTML = "Your roll is " + (spinOneValue);
+
+
     },
     3000
     );
